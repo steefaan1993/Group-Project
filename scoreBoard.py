7c72914db38db5ecf0602cd42bc21bbff88e3f1d
thepsychetron <38162679+thepsychetron@users.noreply.github.com>
2018-11-27 14:41:09 +0000

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDockWidget, QGridLayout,QLabel
+from PyQt5.QtWidgets import QDockWidget, QGridLayout,QLabel, QWidget
 from board import Board
 import sys
 
@@ -14,10 +14,20 @@ class ScoreBoard(QDockWidget):
         self.resize(200, 200)
         self.center()
         self.setWindowTitle('ScoreBoard')
-        self.label= QLabel('Player 1')
-        self.setWidget(self.label)
+        self.labelP1= QLabel('Player 1')
+        self.labelP2 = QLabel('Player 2')
+
+        grid = QGridLayout()
+        grid.addWidget(self.labelP1, 0, 0)
+        grid.addWidget(self.labelP2, 1, 0)
+        
+        self.setLayout(grid)
+
+        self.mainWidget = QWidget()
+        self.mainWidget.setLayout(grid)
+        self.setWidget(self.mainWidget)
+
         self.show()
 
     def center(self):
         '''centers the window on the screen'''
-
