001de0e165d92f708b60168dccbcb198831c966f
steefaan1993 <45168135+steefaan1993@users.noreply.github.com>
2018-11-29 00:42:09 +0100

    Update scoreBoard.py

--- scoreBoard.py
+++ scoreBoard.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QDockWidget, QGridLayout,QLabel, QWidget
+from PyQt5.QtWidgets import QDockWidget, QGridLayout, QLabel, QWidget
+from PyQt5.QtGui import QPixmap
 from board import Board
 import sys
 
@@ -14,13 +15,24 @@ class ScoreBoard(QDockWidget):
         self.resize(200, 200)
         self.center()
         self.setWindowTitle('ScoreBoard')
-        self.labelP1= QLabel('Player 1')
+        self.labelP1 = QLabel('Player 1')
         self.labelP2 = QLabel('Player 2')
 
+        self.avatar1= QPixmap("avatarP1.png")
+        self.avatar1Label=QLabel('')
+        self.avatar1Label.setPixmap(self.avatar1)
+
+        self.avatar2 = QPixmap("avatarP2.png")
+        self.avatar2Label = QLabel('')
+        self.avatar2Label.setPixmap(self.avatar2)
+
         grid = QGridLayout()
-        grid.addWidget(self.labelP1, 0, 0)
-        grid.addWidget(self.labelP2, 1, 0)
-        
+        grid.addWidget(self.avatar1Label, 0, 0)
+        grid.addWidget(self.labelP1, 1, 0)
+
+        grid.addWidget(self.avatar2Label, 2, 0)
+        grid.addWidget(self.labelP2, 3, 0)
+
         self.setLayout(grid)
 
         self.mainWidget = QWidget()
