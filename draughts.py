0ae4a6cb0363317ab2533012aea5c559d7515bb2
thepsychetron <38162679+thepsychetron@users.noreply.github.com>
2018-12-19 19:11:52 +0000

    Update draughts.py

--- draughts.py
+++ draughts.py
@@ -21,13 +21,14 @@ class Draughts(QMainWindow):
 
         # SETTING UP TWO MENUS IN MENU BAR
         gameMenu = mainMenu.addMenu(" Game")
-        optionAction = QAction(QIcon("./img/settings.png"), "Options", self)
-        gameMenu.addAction(optionAction)
-        optionAction.triggered.connect(self.option)
 
         pauseAction = QAction(QIcon("./img/pause.png"), "Pause Game", self)
         gameMenu.addAction(pauseAction)
         pauseAction.triggered.connect(self.tboard.pause)
+
+        playAction = QAction(QIcon("./img/play.png"), "Play Game", self)
+        gameMenu.addAction(playAction)
+        playAction.triggered.connect(self.tboard.start)
 
         resetAction = QAction(QIcon("./img/reset.png"), "Reset", self)
         gameMenu.addAction(resetAction)
@@ -39,9 +40,35 @@ class Draughts(QMainWindow):
 
         helpMenu = mainMenu.addMenu("Help")
         help = helpMenu.addMenu(QIcon("./img/help.png")," Help")
-        help.addAction( "How To Play")
-        help.addAction( "Rules")
-        helpMenu.addAction(QIcon("./img/about.png"), "About")
+        htp = help.addMenu("How To Play")
+
+        rulesAction = QAction( "Rules", self)
+        help.addAction(rulesAction)
+        rulesAction.triggered.connect(self.rules)
+
+        about = QAction(QIcon("./img/about.png"), "About", self)
+        helpMenu.addAction(about)
+        about.triggered.connect(self.about)
+
+        howtoplay = QAction(QIcon("./img/checkers.png"), "How To Play", self)
+        htp.addAction(howtoplay)
+        howtoplay.triggered.connect(self.htpHelp)
+
+        pause = QAction(QIcon("./img/pause.png"), "Pause", self)
+        htp.addAction(pause)
+        pause.triggered.connect(self.pauseHelp)
+
+        play = QAction(QIcon("./img/play.png"), "Play", self)
+        htp.addAction(play)
+        play.triggered.connect(self.playHelp)
+
+        reset = QAction(QIcon("./img/reset.png"), "Reset", self)
+        htp.addAction(reset)
+        reset.triggered.connect(self.resetHelp)
+
+        exit = QAction(QIcon("./img/exit.png"), "Exit", self)
+        htp.addAction(exit)
+        exit.triggered.connect(self.exitHelp)
 
         self.scoreBoard = ScoreBoard()
         self.addDockWidget(Qt.RightDockWidgetArea, self.scoreBoard)
@@ -71,6 +98,8 @@ class Draughts(QMainWindow):
         item, okPressed = QInputDialog.getItem(self, "Options", "Player1 Colour:", items, 0, False)
         if okPressed and item:
             print(item)
+        else:
+            item = "Yellow"
         if item == "Red":
             player1colour = Qt.red
 
@@ -90,6 +119,8 @@ class Draughts(QMainWindow):
         item2, okPressed = QInputDialog.getItem(self, "Options", "Player2 Colour:", items2, 0, False)
         if okPressed and item2:
             print(item2)
+        else:
+            item2 = "Red"
         if item2 == "Red":
             player2colour = Qt.red
         elif item2 == "Blue":
@@ -123,11 +154,62 @@ class Draughts(QMainWindow):
         close = close.exec()
         if close == QMessageBox.Yes:
             event.accept()
-       # elif close == QMessageBox.Save:
-        #    save = self.save()
-         #   if save == False:
-          #      event.ignore()
-           # else:
-            #    event.accept()
         else:
             event.ignore()
+
+    def rules(self):
+        help = QMessageBox()
+        help.setWindowTitle("Rules")
+        help.setWindowIcon(QIcon("./img/checkers.png"))
+        help.setText("1) Each Player start with 12 pieces.\n 2) Pieces can only move diagonally and forward (only one square).\n 3) Captures are made by jumping over an enemy piece.\n 4) If you make a capture, you can keep playing.\n 5) A piece becomes a king if it reaches to the enemy side.\n 6) A king can move forwards and backwards.\n 7) A Player wins if the opponent runs out of time or if the Player captures all 12 enemy pieces in time.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def pauseHelp(self):
+        help = QMessageBox()
+        help.setWindowTitle("Pause")
+        help.setWindowIcon(QIcon("./img/pause.png"))
+        help.setText("This button allows you to pause the game once you started.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def playHelp(self):
+        help = QMessageBox()
+        help.setWindowTitle("Play")
+        help.setWindowIcon(QIcon("./img/play.png"))
+        help.setText("This button allows you to start the game after you pause it.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def resetHelp(self):
+        help = QMessageBox()
+        help.setWindowTitle("Reset")
+        help.setWindowIcon(QIcon("./img/reset.png"))
+        help.setText("This button allows you to reset the timers, the stats, and the position of the pieces.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def exitHelp(self):
+        help = QMessageBox()
+        help.setWindowTitle("Exit")
+        help.setWindowIcon(QIcon("./img/exit.png"))
+        help.setText("This button allows you to exit the application.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def htpHelp(self):
+        help = QMessageBox()
+        help.setWindowTitle("How To Play")
+        help.setWindowIcon(QIcon("./img/checkers.png"))
+        help.setText("If the score board says it's your turn, you can move by clicking on a piece. The application will highlight the available squares to move. You can pause, start, reset and exit by clicking the Game menu in the menu bar. Hope you'll enjoy it!\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
+    def about(self):
+        help = QMessageBox()
+        help.setWindowTitle("About")
+        help.setWindowIcon(QIcon("./img/about.png"))
+        help.setText("This application is a Draughts game (aka Checkers) made by Ecem Oral 2929807 and Stefan Wirtz 2981467.\n")
+        help.setStandardButtons(QMessageBox.Ok)
+        help = help.exec()
+
